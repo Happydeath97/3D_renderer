7aedd3765c105868b87aaee8c3cf0cc4fe583674
Happydeath97 <KrystofVavra97@seznam.cz>
2025-10-23 14:47:31 +0200

    final version

--- main.cpp
+++ main.cpp
@@ -50,6 +50,66 @@ static Matrix MatrixRotateYCustom(float angle) {
     return m;
 }
 
+static Vector3 Vector3TransformCustom(Vector3 v, Matrix m) {
+    Vector3 result = { 0 };
+    result.x = v.x*m.m0 + v.y*m.m4 + v.z*m.m8 + m.m12;
+    result.y = v.x*m.m1 + v.y*m.m5 + v.z*m.m9 + m.m13;
+    result.z = v.x*m.m2 + v.y*m.m6 + v.z*m.m10 + m.m14;
+    return result;
+}
+
+typedef struct MyCube {
+    Vector3 vertices[8];
+    Vector3 transformed[8];
+    Matrix transform;
+} MyCube;
+
+MyCube InitMyCube(float width, float height, float depth) {
+    MyCube cube = { 0 };
+
+    float x = width / 2.0f;
+    float y = height / 2.0f;
+    float z = depth / 2.0f;
+
+    cube.vertices[0] = (Vector3){ -x,  y, -z };
+    cube.vertices[1] = (Vector3){  x,  y, -z };
+    cube.vertices[2] = (Vector3){  x, -y, -z };
+    cube.vertices[3] = (Vector3){ -x, -y, -z };
+    cube.vertices[4] = (Vector3){ -x,  y,  z };
+    cube.vertices[5] = (Vector3){  x,  y,  z };
+    cube.vertices[6] = (Vector3){  x, -y,  z };
+    cube.vertices[7] = (Vector3){ -x, -y,  z };
+
+    cube.transform = MatrixIdentityCustom();
+
+    return cube;
+}
+
+void UpdateTransformedVertices(MyCube *cube) {
+    for (int i = 0; i < 8; i++) {
+        cube->transformed[i] = Vector3TransformCustom(cube->vertices[i], cube->transform);
+    }
+}
+
+void DrawMyCube(MyCube cube) {
+    Vector3 *v = cube.transformed;
+
+    DrawLine3D(v[0], v[1], RED);
+    DrawLine3D(v[1], v[2], RED);
+    DrawLine3D(v[2], v[3], RED);
+    DrawLine3D(v[3], v[0], RED);
+
+    DrawLine3D(v[4], v[5], RED);
+    DrawLine3D(v[5], v[6], RED);
+    DrawLine3D(v[6], v[7], RED);
+    DrawLine3D(v[7], v[4], RED);
+
+    DrawLine3D(v[0], v[4], RED);
+    DrawLine3D(v[1], v[5], RED);
+    DrawLine3D(v[2], v[6], RED);
+    DrawLine3D(v[3], v[7], RED);
+}
+
 // --- Main program ---
 int main(void) {
     const int screenWidth = 800;
@@ -57,15 +117,19 @@ int main(void) {
     InitWindow(screenWidth, screenHeight, "Rotate cube with mouse drag (pure raylib + custom math)");
 
     Camera3D camera = { 0 };
-    camera.position = (Vector3){ 6.0f, 6.0f, 6.0f };
+    camera.position = (Vector3){ 0.0f, 0.0f, 6.0f };
     camera.target   = (Vector3){ 0.0f, 0.0f, 0.0f };
     camera.up       = (Vector3){ 0.0f, 1.0f, 0.0f };
     camera.fovy     = 45.0f;
     camera.projection = CAMERA_PERSPECTIVE;
 
-    Mesh cube = GenMeshCube(2.0f, 2.0f, 2.0f);
-    Model cubeModel = LoadModelFromMesh(cube);
-    cubeModel.materials[0].maps[MATERIAL_MAP_DIFFUSE].color = RED;
+    // using inbuild cube mesh
+    //Mesh cube = GenMeshCube(2.0f, 2.0f, 2.0f);
+    //Model cubeModel = LoadModelFromMesh(cube);
+    //cubeModel.materials[0].maps[MATERIAL_MAP_DIFFUSE].color = RED;
+
+    // trying to use my own
+    MyCube cube2 = InitMyCube(2, 2, 2);
 
     float rotationX = 0.0f;
     float rotationY = 0.0f;
@@ -83,14 +147,18 @@ int main(void) {
         // Build transform matrix manually
         Matrix rotX = MatrixRotateXCustom(rotationX);
         Matrix rotY = MatrixRotateYCustom(rotationY);
-        cubeModel.transform = MatrixMultiplyCustom(rotX, rotY);
+        Matrix rotation = MatrixMultiplyCustom(rotX, rotY);
+        //cubeModel.transform = rotation;
+        cube2.transform = rotation;
+        UpdateTransformedVertices(&cube2);
 
         // Draw
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
         BeginMode3D(camera);
-            DrawModel(cubeModel, (Vector3){0,0,0}, 1.0f, WHITE);
+            //DrawModel(cubeModel, (Vector3){0,0,0}, 1.0f, WHITE);
+            DrawMyCube(cube2);
         EndMode3D();
 
         DrawText("Drag left mouse button to rotate cube", 10, 10, 20, DARKGRAY);
@@ -98,7 +166,7 @@ int main(void) {
         EndDrawing();
     }
 
-    UnloadModel(cubeModel);
+    //UnloadModel(cubeModel);
     CloseWindow();
     return 0;
 }
